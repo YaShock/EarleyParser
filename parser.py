b6d571f0cbd940c01ece1a7cd8fd393b494667a7
YaShock <jashok12@gmail.com>
2017-06-02 22:57:36 +0200

    Added Tree class

--- parser.py
+++ parser.py
@@ -135,6 +135,22 @@ class State(object):
             return None
         return self.rule.production[self.dotPos]
 
+class Tree(object):
+    def __init__(self, data=None, children=None):
+        self.children = []
+        if children is not None:
+            self.children.extend(children)
+        self.data = data
+    def add(self, child):
+        self.children.append(child)
+    def print(self, level=0):
+        if level == 9:
+            return
+        text = "\t"*level+str(self.data)
+        print(text)
+        for child in self.children:
+            child.print(level+1)
+
 class EarleyParser(object):
     """docstring for EarleyParser"""
     def __init__(self, topRule):
@@ -179,20 +195,11 @@ class EarleyParser(object):
             print()
             for state in self.state_list[k]:
                 print(state)
-        print("\nFinal completed states")
+        result = []
         for st in self.state_list[-1]:
             if st.rule == self.topRule:
-                print(st)
-                self.print_state_tree(st)
-                print()
-        # print("\nChoosen:")
-        # for st in self.state_list[-1]:
-        #     if st.rule == self.topRule:
-        #         e = st
-        #         break
-        # if self.state_list[-1]:
-        #     self.print_state_tree(e)
-        #     return e
+                result.append(self.construct_tree(st))
+        return result
 
     def is_terminal(self, token):
         return token in self.terminals
@@ -208,7 +215,6 @@ class EarleyParser(object):
         if state.next() == self.tokens[k]:
             self.state_list[k+1].add(State(copy.deepcopy(state.rule), state.dotPos+1, state.orig_pos, k+1, state.back_pointers))
 
-
     def complete(self, state, k):
         for s in self.state_list[state.orig_pos]:
             if s.next() == state.rule.variable:
@@ -216,31 +222,121 @@ class EarleyParser(object):
                 newState.back_pointers.append(state)
                 self.state_list[k].add(newState)
 
-    def print_state_tree(self, state, level=0):
+    def read_file(self, file):
+        for line in file:
+            print(line)
+
+    def __parse_rule(self, line):
+        l = line.split('->')
+
+
+    def construct_tree(self, state, level=0):
+        tree = Tree()
+        tree.data = repr(state.rule.variable)
         j = 0
         for i in range(len(state.rule.production)):
-            text = "\t"*level+str(state.rule.production[i])
-            print(text)
+            node = Tree()
             if state.back_pointers:
                 if not self.is_terminal(state.rule.production[i]):
-                    self.print_state_tree(state.back_pointers[j], level+1)
+                    node = self.construct_tree(state.back_pointers[j], level+1)
                     j += 1
+            node.data = repr(state.rule.production[i])
+            tree.add(node)
+        return tree
 
 
-P = Variable("P")
-S = Variable("S")
-M = Variable("M")
-T = Variable("T")
+GAMMA = Variable("GAMMA")
+Command = Variable("Command")
+Transform = Variable("Transform")
+Generate = Variable("Generate")
+LoadFile = Variable("LoadFile")
+Modify = Variable("Modify")
+Request = Variable("Request")
+TextObject = Variable("TextObject")
+ModTerm = Variable("ModTerm")
+DelTerm = Variable("DelTerm")
+ReqTerm = Variable("ReqTerm")
+TrasformFunction = Variable("TrasformFunction")
+FunctionLower = Variable("FunctionLower")
+FunctionUpper = Variable("FunctionUpper")
+Delete = Variable("Delete")
+Object = Variable("Object")
+WordList = Variable("WordList")
+W = Variable("W")
+SparseWords = Variable("SparseWords")
+FreqWords = Variable("FreqWords")
+Stopwords = Variable("Stopwords")
+Language = Variable("Language")
+WordTerm = Variable("WordTerm")
+GenerateTerm = Variable("GenerateTerm")
+WhiteSpaces = Variable("WhiteSpaces")
 
-parser = EarleyParser(Rule(P, Production(S)))
+parser = EarleyParser(Rule(GAMMA, Production(Command)))
 
-parser.add(Rule(S, Production(S, '+', M)))
-parser.add(Rule(S, Production(M)))
-parser.add(Rule(M, Production(M, '*', T)))
-parser.add(Rule(M, Production(T)))
-parser.add(Rule(T, Production('1')))
-parser.add(Rule(T, Production('2')))
-parser.add(Rule(T, Production('3')))
-parser.add(Rule(T, Production('4')))
+parser.add(Rule(Command, Production(Transform)))
+parser.add(Rule(Command, Production(Generate)))
+parser.add(Rule(Command, Production(Request)))
+parser.add(Rule(Command, Production(LoadFile)))
 
-parser.parse("2 + 3 * 4")
+parser.add(Rule(LoadFile, Production('load', StringTerm())))
+parser.add(Rule(Generate, Production(GenerateTerm, 'word', 'cloud')))
+
+parser.add(Rule(Transform, Production(Modify)))
+parser.add(Rule(Transform, Production(Delete)))
+
+parser.add(Rule(Modify, Production(ModTerm, TextObject, "to", TrasformFunction)))
+parser.add(Rule(Modify, Production(ModTerm, "to", TrasformFunction)))
+parser.add(Rule(TrasformFunction, Production(FunctionLower)))
+parser.add(Rule(TrasformFunction, Production(FunctionUpper)))
+parser.add(Rule(FunctionLower, Production("lower", "case")))
+parser.add(Rule(FunctionLower, Production("lower")))
+parser.add(Rule(FunctionUpper, Production("upper", "case")))
+parser.add(Rule(FunctionUpper, Production("upper")))
+
+parser.add(Rule(Delete, Production(DelTerm, Object)))
+parser.add(Rule(Request, Production(ReqTerm, Object)))
+
+parser.add(Rule(Object, Production("numbers")))
+parser.add(Rule(Object, Production("punctuation")))
+parser.add(Rule(Object, Production(Stopwords)))
+parser.add(Rule(Object, Production(WordList)))
+parser.add(Rule(Object, Production(WhiteSpaces)))
+parser.add(Rule(Object, Production(SparseWords)))
+parser.add(Rule(Object, Production(FreqWords)))
+parser.add(Rule(SparseWords, Production(WordTerm, "sparsity", FloatTerm())))
+parser.add(Rule(FreqWords, Production(WordTerm, "frequency", "between", FloatTerm(), "and", FloatTerm())))
+parser.add(Rule(Stopwords, Production(Language, "stopwords")))
+parser.add(Rule(Stopwords, Production(Language, "stop", "words")))
+parser.add(Rule(Language, Production("english")))
+parser.add(Rule(Language, Production("german")))
+parser.add(Rule(Language, Production("hungarian")))
+parser.add(Rule(TextObject, Production("text")))
+parser.add(Rule(TextObject, Production("document")))
+parser.add(Rule(ModTerm, Production('modify')))
+parser.add(Rule(ModTerm, Production('transform')))
+parser.add(Rule(ModTerm, Production('convert')))
+parser.add(Rule(DelTerm, Production('delete')))
+parser.add(Rule(DelTerm, Production('remove')))
+parser.add(Rule(DelTerm, Production('strip')))
+parser.add(Rule(DelTerm, Production('take', 'out')))
+parser.add(Rule(ReqTerm, Production('get')))
+parser.add(Rule(ReqTerm, Production('find')))
+parser.add(Rule(ReqTerm, Production('what', 'are')))
+parser.add(Rule(ReqTerm, Production('what', 'is')))
+parser.add(Rule(WordTerm, Production('words')))
+parser.add(Rule(WordTerm, Production('terms')))
+parser.add(Rule(GenerateTerm, Production('create')))
+parser.add(Rule(GenerateTerm, Production('generate')))
+parser.add(Rule(GenerateTerm, Production('make')))
+parser.add(Rule(WhiteSpaces, Production('white', 'space')))
+parser.add(Rule(WhiteSpaces, Production('whitespace')))
+parser.add(Rule(WordList, Production(WordTerm, W)))
+parser.add(Rule(W, Production(W, W)))
+parser.add(Rule(W, Production(StringTerm())))
+
+lst = parser.parse("convert to upper case")
+for t in lst:
+    t.print()
+
+file = open('cf.txt')
+parser.read_file(file)
